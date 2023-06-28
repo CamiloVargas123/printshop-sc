f1eccecd0b49ec1c24e57dc4918affb5525ab37e
Camilo Vargas <camilovargasingm@gmail.com>
2023-06-28 12:49:10 -0500

    Feat: create and add mobileNav

--- src/components/layouts/SiteHeader.tsx
+++ src/components/layouts/SiteHeader.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link'
 import { buttonVariants } from '@/components/ui/Button'
 import { siteConfig } from '@/config/site'
 import MainNav from './MainNav'
+import MobileNav from './MobileNav'
 
 export default function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background border-border">
       <div className="container flex h-14 items-center">
         <MainNav items={siteConfig.mainNav} />
+        <MobileNav mainNavItems={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
             <Link href="/signin" className={buttonVariants({ size: "sm" })}>
