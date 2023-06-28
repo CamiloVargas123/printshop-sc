e9c93e16d6955e01d69bcbcdb06a49056b486e05
Camilo Vargas <camilovargasingm@gmail.com>
2023-06-28 12:16:32 -0500

    Feat: Create desktop header navigation

--- src/components/layouts/SiteHeader.tsx
+++ src/components/layouts/SiteHeader.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
-import React from 'react'
 import { buttonVariants } from '@/components/ui/Button'
+import { siteConfig } from '@/config/site'
+import MainNav from './MainNav'
 
 export default function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background border-border">
       <div className="container flex h-14 items-center">
+        <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-2">
             <Link href="/signin" className={buttonVariants({ size: "sm" })}>
