ec0abf5cac9358dcd9faa6bf0484f89265147a46
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-04 20:37:22 -0500

    Fix: removed link wrapper component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -21,47 +21,47 @@ interface ProductCardProps {
 
 export function ProductCard({ product, categoryPath }: ProductCardProps) {
   return (
-    <Link
-      aria-label={`Ver ${product.title} detalle`}
-      href={`${categoryPath}/${product.slug}`}
-    >
-      <Card className="h-full overflow-hidden rounded-sm">
-        <CardHeader className="border-b p-0">
-          <AspectRatio ratio={4 / 3}>
-            {product.images.length ? (
-              <Image
-                src={product.images[0].url}
-                alt={product.images[0].name}
-                fill
-                className="object-cover"
-                loading="lazy"
+    <Card className="h-full overflow-hidden rounded-sm">
+      <CardHeader className="border-b p-0">
+        <AspectRatio ratio={4 / 3}>
+          {product.images.length ? (
+            <Image
+              src={product.images[0].url}
+              alt={product.images[0].name}
+              fill
+              className="object-cover"
+              loading="lazy"
+            />
+          ) : (
+            <div className="flex h-full items-center justify-center bg-muted">
+              <Icons.placeholder
+                className="h-9 w-9 text-muted-foreground"
+                aria-hidden="true"
               />
-            ) : (
-              <div className="flex h-full items-center justify-center bg-secondary">
-                <Icons.placeholder
-                  className="h-9 w-9 text-muted-foreground"
-                  aria-hidden="true"
-                />
-              </div>
-            )}
-          </AspectRatio>
-        </CardHeader>
-        <CardContent className="grid gap-2.5 p-4">
-          <CardTitle>{product.title}</CardTitle>
-          <CardDescription className="line-clamp-2">
-            {`Desde: ${formatPrice(product.price ?? 0)}`}
-          </CardDescription>
-        </CardContent>
-        <CardFooter className="p-4">
-          <div className={buttonVariants({
+            </div>
+          )}
+        </AspectRatio>
+      </CardHeader>
+      <CardContent className="grid gap-2.5 p-4">
+        <CardTitle>{product.title}</CardTitle>
+        <CardDescription className="line-clamp-2">
+          {`Desde: ${formatPrice(product.price ?? 0)}`}
+        </CardDescription>
+      </CardContent>
+      <CardFooter className="p-4">
+        <Link
+          aria-label={`Ver ${product.title} detalle`}
+          href={`${categoryPath}/${product.slug}`}
+          prefetch={false}
+          className={buttonVariants({
             variant: "secondary",
             size: "sm",
             className: "h-8 w-full rounded-sm",
-          })}>
-            Seleccionar
-          </div>
-        </CardFooter>
-      </Card>
-    </Link>
+          })}
+        >
+          Seleccionar
+        </Link>
+      </CardFooter>
+    </Card>
   )
 }
