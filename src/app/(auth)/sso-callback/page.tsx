d4a2c719efe1bf06e58486daa6e1aba6eb9350a3
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-05 23:28:58 -0500

    Feat: Created page SSO-callback

---
+++ src/app/(auth)/sso-callback/page.tsx
@@ -0,0 +1,18 @@
+import { Shell } from "@/components/Shell"
+import { type HandleOAuthCallbackParams } from "@clerk/types"
+import SSOCallback from "./components/SSOCallback"
+
+// Running out of edge function execution units on vercel free plan
+// export const runtime = "edge"
+
+export interface SSOCallbackPageProps {
+  searchParams: HandleOAuthCallbackParams
+}
+
+export default function SSOCallbackPage({ searchParams }: SSOCallbackPageProps) {
+  return (
+    <Shell layout="auth">
+      <SSOCallback searchParams={searchParams} />
+    </Shell>
+  )
+}
