3ba3fc3c5a8eead2b75b678614e3c73c757113e4
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-10 12:53:27 -0500

    Feat: saved cartItems into localstorage

--- src/redux/slices/cart.ts
+++ src/redux/slices/cart.ts
@@ -2,12 +2,17 @@ import { ProductCart } from "@/models"
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"
 import { RootState } from "../store"
 
+const cart = localStorage.getItem("cart") !== null ? JSON.parse(localStorage.getItem("cart") || "") : null
+function saveCartToLocalStorage(cart: InitialState) {
+  localStorage.setItem("cart", JSON.stringify(cart))
+}
+
 interface InitialState {
   items: Array<ProductCart>
   amountAll: number
   subTotal: number
 }
-const initialState: InitialState = {
+const initialState: InitialState = cart ?? {
   items: [],
   amountAll: 0,
   subTotal: 0
@@ -21,11 +26,13 @@ export const cartSlice = createSlice({
       state.items = state.items.concat({ ...action.payload })
       state.subTotal += action.payload.price
       state.amountAll += 1
+      saveCartToLocalStorage({ ...state })
     },
     removeToCart: (state, action: PayloadAction<ProductCart>) => {
       state.items = state.items.filter(_product => _product.id != action.payload.id)
       state.subTotal -= action.payload.price
       state.amountAll -= 1
+      saveCartToLocalStorage({ ...state })
     }
   }
 })
