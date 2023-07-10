801080698671c645b51b1635a34c6ca4ec5c75cc
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-10 17:30:45 -0500

    Feat: created form of personal data for send the buy

---
+++ src/app/(lobby)/checkout/components/PersonalDataForm.tsx
@@ -0,0 +1,134 @@
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/Form'
+import { Input } from '@/components/ui/Input'
+import { Textarea } from '@/components/ui/Textarea'
+import { UseFormReturn } from 'react-hook-form'
+import { CheckoutInputs } from '../models'
+
+interface PersonalDataFormProps {
+  form: UseFormReturn<CheckoutInputs>
+}
+
+export default function PersonalDataForm({ form }: PersonalDataFormProps) {
+  return (
+    <div className='flex flex-col gap-4'>
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel required>Nombre completo (nombre y apellido)</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="telephone"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel required>Número de teléfono</FormLabel>
+            <FormControl>
+              <Input placeholder="+34 912345678" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="city"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel required>Ciudad</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="state"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel required>Estado / Provincia / Región</FormLabel>
+            <FormControl>
+              <Input {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <div className='flex flex-col gap-2'>
+        <FormField
+          control={form.control}
+          name="address"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel required>Dirección</FormLabel>
+              <FormControl>
+                <Input placeholder='Nombre de la calle' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="addressOptional"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder='Depto., unidad, edificio, piso, etc.' {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+      <FormField
+        control={form.control}
+        name="postalCode"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel required>Código Postal</FormLabel>
+            <FormControl>
+              <Input placeholder="28001" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Email</FormLabel>
+            <FormControl>
+              <Input placeholder="example@hotmail.com" {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+      <FormField
+        control={form.control}
+        name="note"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Información adicional</FormLabel>
+            <FormControl>
+              <Textarea {...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  )
+}
