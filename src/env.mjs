6a8901a68204e0df5c8aada02c5d73b176513831
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-12 23:15:21 -0500

    Feat: integrate firebase database and storage

--- src/env.mjs
+++ src/env.mjs
@@ -5,6 +5,13 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     CLERK_SECRET_KEY: z.string().min(1),
+
+    FIREBASE_APP_ID: z.string().min(1),
+    FIREBASE_API_KEY: z.string().min(1),
+    FIREBASE_AUTH_DOMAIN: z.string().min(1),
+    FIREBASE_PROJECT_ID: z.string().min(1),
+    FIREBASE_STORAGE_BUCKET: z.string().min(1),
+    FIREBASE_MESSAGING_SENDER_ID: z.string().min(3).regex(/^\d+$/),
   },
   client: {
     NEXT_PUBLIC_NODE_ENV: z.enum(["development", "test", "production"]),
@@ -19,5 +26,12 @@ export const env = createEnv({
     NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
+
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
   }
 })
---
+++ src/server/config.ts
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { env } from "@/env.mjs";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { db, storage }
