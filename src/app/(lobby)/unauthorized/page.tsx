e311899928236704a14be8a80e14753eee3aaf15
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-13 16:16:50 -0500

    Feat: added page of unauthorized

---
+++ src/app/(lobby)/unauthorized/page.tsx
@@ -0,0 +1,9 @@
+
+export default function UnauthorizedPage() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-[50vh]">
+      <h3 className="font-bold text-2xl">No estás autorizado para acceder a esta página</h3>
+      <p>Por favor, inicia sesión o contacta al administrador del sitio para obtener acceso.</p>
+    </div>
+  )
+}
