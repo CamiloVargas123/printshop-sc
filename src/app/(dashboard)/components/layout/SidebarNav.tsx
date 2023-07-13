83ed7132412ac6a3d084e3601557dcb83a32f0b0
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-13 15:36:39 -0500

    Feat: created sidebar of dashboard

---
+++ src/app/(dashboard)/components/layout/SidebarNav.tsx
@@ -0,0 +1,54 @@
+"use client"
+
+import type { SidebarNavItem } from "@/models"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { Icons } from "@/components/Icons"
+import { cn } from "@/lib/utils"
+
+export interface SidebarNavProps {
+  items: SidebarNavItem[]
+}
+
+export default function SidebarNav({ items }: SidebarNavProps) {
+  const pathname = usePathname()
+
+  if (!items?.length) return null
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      {items.map((item, index) => {
+        const Icon = Icons[item.icon ?? "chevronLeft"]
+
+        return item.href ? (
+          <Link
+            key={index}
+            href={item.href}
+            target={item.external ? "_blank" : ""}
+            rel={item.external ? "noreferrer" : ""}
+          >
+            <span
+              className={cn(
+                "group flex w-full items-center rounded-sm border border-transparent px-2 py-1 hover:bg-muted hover:text-foreground",
+                pathname === item.href
+                  ? "bg-muted font-medium text-foreground"
+                  : "text-muted-foreground",
+                item.disabled && "pointer-events-none opacity-60"
+              )}
+            >
+              <Icon className="mr-2 h-4 w-4" aria-hidden="true" />
+              <span>{item.title}</span>
+            </span>
+          </Link>
+        ) : (
+          <span
+            key={index}
+            className="flex w-full cursor-not-allowed items-center rounded-sm p-2 text-muted-foreground hover:underline"
+          >
+            {item.title}
+          </span>
+        )
+      })}
+    </div>
+  )
+}
