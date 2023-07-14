45a029bd357d9a6da7465c1a98c1a33af6de274d
Camilo Vargas <camilovargasingm@gmail.com>
2023-07-13 21:54:03 -0500

    Feat: create uncrontolled input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,15 +4,12 @@ import { useFormField } from "./Form"
 
 export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
-const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    const { error } = useFormField()
+const InputDefault = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
     return (
       <input
-        type={type}
         className={cn(
           "flex h-10 w-full rounded-sm border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          error && "border-destructive bg-destructive/20",
           className
         )}
         ref={ref}
@@ -21,7 +18,27 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     )
   }
 )
+InputDefault.displayName = "InputDefault"
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    const { error } = useFormField()
+    return <InputDefault
+      ref={ref}
+      className={cn(
+        className,
+        error && "border-destructive bg-destructive/20",
+      )} {...props} />
+  }
+)
 Input.displayName = "Input"
 
-export { Input }
+const InputUncontrolled = forwardRef<HTMLInputElement, InputProps>(
+  ({ ...props }, ref) => {
+    return <InputDefault ref={ref} {...props} />
+  }
+)
+InputUncontrolled.displayName = "InputUncontrolled"
+
+export { Input, InputUncontrolled }
 
